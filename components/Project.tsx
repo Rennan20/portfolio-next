107059af7134232b33c4150e811a551860a64360
Rennan Silva <jrennan99@gmail.com>
2023-05-19 12:45:53 -0300

    adding vista-geek

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,14 +3,15 @@ import { ProjectType } from "@/typing";
 export const projects: ProjectType[] = [
   {
     id: 1,
-    /* src: "/landing-page-desktop-removebg-preview.png", */
-    src: "https://i.imgur.com/hubBoDI.png",
-    alt: "Landing-page",
-    name: "Template Portfólio",
-    description: "Portfólio de programação customizavél",
-    link: "https://next-portfolio-template-rho.vercel.app/",
+    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/CtZxGFx.png",
+    alt: "Vista-geek",
+    name: "E-commerce",
+    description: "Template de um e-commerce de roupas",
+    link: "https://vista-geek.vercel.app/",
     type: 1,
   },
+
   {
     id: 2,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
@@ -23,6 +24,17 @@ export const projects: ProjectType[] = [
   },
   {
     id: 3,
+    /* src: "/landing-page-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/hubBoDI.png",
+    alt: "Landing-page",
+    name: "Template Portfólio",
+    description: "Portfólio de programação customizavél",
+    link: "https://next-portfolio-template-rho.vercel.app/",
+    type: 3,
+  },
+
+  {
+    id: 4,
     /*     src: "/portfolio-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/0kbw5Ep.png",
     alt: "Portfolio",
@@ -30,19 +42,19 @@ export const projects: ProjectType[] = [
     description:
       "Meu primeiro portfólio, lá estão vários projetos mais básicos e é um bom comparativo da minha evolução!",
     link: "https://rennan20.github.io/meu-portfolio/",
-    type: 3,
+    type: 4,
   },
   {
-    id: 4,
+    id: 5,
     src: "https://i.imgur.com/rz9fUdf.png",
     alt: "Calculadora IMC",
     name: "Calculadora IMC",
     description: "Calculadora de IMC simples feita em React com Vite.",
     link: "https://react-calc-imc-snowy.vercel.app/",
-    type: 4,
+    type: 5,
   },
   {
-    id: 5,
+    id: 6,
     /*   src: "/rpg-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/CJci8qK.png",
     alt: "RPG",
@@ -50,10 +62,10 @@ export const projects: ProjectType[] = [
     description:
       "Interação de diversos elementos no estilo RPG, feito em React",
     link: "https://rpg-tag.vercel.app/",
-    type: 5,
+    type: 6,
   },
   {
-    id: 6,
+    id: 7,
     /*  src: "/nft-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/6Pi9w4k.png",
     alt: "NFT Droper",
@@ -61,10 +73,10 @@ export const projects: ProjectType[] = [
     description:
       "Aplicativo para gerar NFT, o usuário pode logar com MetaMask e gerar uma NFT da coleção salva no Sanity e ThirdWeb",
     link: "https://nft-drop-mu-lilac.vercel.app/",
-    type: 6,
+    type: 7,
   },
   {
-    id: 7,
+    id: 8,
     /*  src: "/chat-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/EQH5IAg.png",
     alt: "ChatGPT",
@@ -72,10 +84,10 @@ export const projects: ProjectType[] = [
     description:
       "Clone do chatGPT usando a API da OpenAI, o usuário pode fazer login com Google, os dados são armazenados com Firebase",
     link: "https://chatgpt-clone-hazel.vercel.app/",
-    type: 7,
+    type: 8,
   },
   {
-    id: 8,
+    id: 9,
     /*  src: "/netflix-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/9yKXCOX.png",
 
@@ -84,16 +96,16 @@ export const projects: ProjectType[] = [
     description:
       "Clone da Netflix, onde o usuário pode se cadastrar e acessar a tela de filmes, utiliza Firebase para manter os dados de login e carrega os filmes através de uma API externa",
     link: "https://netflix-clone-51cb3.web.app/",
-    type: 8,
+    type: 9,
   },
   {
-    id: 9,
+    id: 10,
     src: "https://i.imgur.com/qUco6UV.png",
     alt: "Gerenciador Financeiro",
     name: "Manager",
     description:
       "Aplicativo onde o usuário pode fazer o gerenciamento dos seus gastos e calcular o total no mês, feito com React",
     link: "https://expense-manager-beryl.vercel.app/",
-    type: 9,
+    type: 10,
   },
 ];
