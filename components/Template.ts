5a0d62fb020553b6186dcfd405c08d9f6865056d
Rennan Silva <jrennan99@gmail.com>
2023-05-31 13:30:57 -0300

    adding barberaria

--- components/Template.ts
+++ components/Template.ts
@@ -14,24 +14,35 @@ export const templates: ProjectType[] = [
   {
     id: 3,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
-    src: "https://i.imgur.com/ZEFovSB.png",
-    alt: "portfólio",
-    name: "Portfólio marketing",
-    description:
-      "Template de um site de divulgação da área de marketing digital",
-    link: "https://portfolio-canva.vercel.app/",
-    type: 3,
-  },
-
-  {
-    id: 2,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/xGmSfb7.png",
     alt: "eletronics",
     name: "Loja de eletrônicos",
     description:
       "Template de divulgação de um e-commerce de produtos eletrônicos",
     link: "https://eletronics-shop.vercel.app/",
+    type: 3,
+  },
+  {
+    id: 4,
+    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/ZEFovSB.png",
+    alt: "portfólio",
+    name: "Portfólio marketing",
+    description:
+      "Template de um site de divulgação da área de marketing digital",
+    link: "https://portfolio-canva.vercel.app/",
+    type: 4,
+  },
+  {
+    id: 2,
+    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/iJvPW9q.png",
+    alt: "barbearia",
+    name: "Template Barbearia",
+    description: "Template de um site de barbearia com sistema de agendamento",
+    link: "https://barber-shop-sigma.vercel.app/",
     type: 2,
   },
+
+  /* https://i.imgur.com/iJvPW9q.png */
 ];
