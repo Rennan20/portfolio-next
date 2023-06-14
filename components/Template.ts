9f823e5feccc92068d8a81b9e8fbae94397fa1d1
Rennan Silva <jrennan99@gmail.com>
2023-06-14 13:10:24 -0300

    adding e-book

--- components/Template.ts
+++ components/Template.ts
@@ -3,26 +3,36 @@ import { ProjectType } from "@/typing";
 export const templates: ProjectType[] = [
   {
     id: 1,
+    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/jZg1jmy.png ",
+    alt: "e-book",
+    name: "Landing Page E-book",
+    description: "Landing page do meu e-book JavaScript Básico Simplificado",
+    link: "https://e-book-lovat.vercel.app/",
+    type: 1,
+  },
+  {
+    id: 2,
     /* src: "/landing-page-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/7bqasz1.png",
     alt: "fitness",
     name: "Landing Page Academia",
     description: "Template para divulgação de uma academia",
     link: "https://fitness-site-one.vercel.app/",
-    type: 1,
+    type: 2,
   },
   {
-    id: 2,
+    id: 3,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/iJvPW9q.png",
     alt: "barbearia",
     name: "Template Barbearia",
     description: "Template de um site de barbearia com sistema de agendamento",
     link: "https://barber-shop-sigma.vercel.app/",
-    type: 2,
+    type: 3,
   },
   {
-    id: 3,
+    id: 4,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/xGmSfb7.png",
     alt: "eletronics",
@@ -30,21 +40,21 @@ export const templates: ProjectType[] = [
     description:
       "Template de divulgação de um e-commerce de produtos eletrônicos",
     link: "https://eletronics-shop.vercel.app/",
-    type: 3,
+    type: 4,
   },
   {
-    id: 4,
+    id: 5,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/UjWV1vb.png",
     alt: "portfólio",
     name: "Landing Page Advogado",
     description: "Template de um site de divulgação da área de advocacia",
     link: "https://advogado-landing-page-seven.vercel.app/",
-    type: 4,
+    type: 5,
   },
 
   {
-    id: 5,
+    id: 6,
     /*   src: "/recipes-site-desktop-removebg-preview.png", */
     src: "https://i.imgur.com/ZEFovSB.png",
     alt: "portfólio",
@@ -52,7 +62,7 @@ export const templates: ProjectType[] = [
     description:
       "Template de um site de divulgação da área de marketing digital",
     link: "https://portfolio-canva.vercel.app/",
-    type: 5,
+    type: 6,
   },
 
   /* https://i.imgur.com/iJvPW9q.png */
