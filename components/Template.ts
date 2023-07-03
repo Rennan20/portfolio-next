45723b8e2925e1c686d1f22c8d92b799742736c7
Rennan Silva <jrennan99@gmail.com>
2023-07-03 09:05:53 -0300

    adding tatuagem

--- components/Template.ts
+++ components/Template.ts
@@ -3,8 +3,7 @@ import { ProjectType } from "@/typing";
 export const templates: ProjectType[] = [
   {
     id: 1,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
-    src: "https://i.imgur.com/jZg1jmy.png ",
+    src: "https://i.imgur.com/jZg1jmy.png",
     alt: "e-book",
     name: "Landing Page E-book",
     description: "Landing page do meu e-book JavaScript Básico Simplificado",
@@ -13,68 +12,67 @@ export const templates: ProjectType[] = [
   },
   {
     id: 2,
-    /* src: "/landing-page-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/vJmGRPx.png",
+    alt: "estúdio",
+    name: "Landing Page Estúdio",
+    description: "Landing page estúdio de tatuagem",
+    link: "https://landing-page-tatuage-v-2.vercel.app/",
+    type: 2,
+  },
+  {
+    id: 3,
     src: "https://i.imgur.com/7bqasz1.png",
     alt: "fitness",
     name: "Landing Page Academia",
     description: "Template para divulgação de uma academia",
     link: "https://fitness-site-one.vercel.app/",
-    type: 2,
+    type: 3,
   },
   {
-    id: 3,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    id: 4,
     src: "https://i.imgur.com/iJvPW9q.png",
     alt: "barbearia",
     name: "Template Barbearia",
     description: "Template de um site de barbearia com sistema de agendamento",
     link: "https://barber-shop-sigma.vercel.app/",
-    type: 3,
-  },
-  /*https://i.imgur.com/rEa6fc5.png*/
-  {
-    id: 4,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
-    src: "https://i.imgur.com/rEa6fc5.png",
-    alt: "landing page",
-    name: "Landing Page",
-    description: "Landin Pages de aulas online",
-    link: "https://landing-page-aulas.vercel.app/",
     type: 4,
   },
   {
     id: 5,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    src: "https://i.imgur.com/rEa6fc5.png",
+    alt: "landing page",
+    name: "Landing Page",
+    description: "Landing Pages de aulas online",
+    link: "https://landing-page-aulas.vercel.app/",
+    type: 5,
+  },
+  {
+    id: 6,
     src: "https://i.imgur.com/xGmSfb7.png",
     alt: "eletronics",
     name: "Loja de eletrônicos",
     description:
       "Template de divulgação de um e-commerce de produtos eletrônicos",
     link: "https://eletronics-shop.vercel.app/",
-    type: 5,
+    type: 6,
   },
   {
-    id: 6,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    id: 7,
     src: "https://i.imgur.com/UjWV1vb.png",
     alt: "portfólio",
     name: "Landing Page Advogado",
     description: "Template de um site de divulgação da área de advocacia",
     link: "https://advogado-landing-page-seven.vercel.app/",
-    type: 6,
+    type: 7,
   },
-
   {
-    id: 7,
-    /*   src: "/recipes-site-desktop-removebg-preview.png", */
+    id: 8,
     src: "https://i.imgur.com/ZEFovSB.png",
     alt: "portfólio",
     name: "Portfólio marketing",
     description:
       "Template de um site de divulgação da área de marketing digital",
     link: "https://portfolio-canva.vercel.app/",
-    type: 7,
+    type: 8,
   },
-
-  /* https://i.imgur.com/iJvPW9q.png */
 ];
